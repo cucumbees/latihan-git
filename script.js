d941be174af09c05bebd4fbe8b309529463f8dfe
Juan Darmawan <jubanpre@gmail.com>
2025-11-29 10:35:21 +0700

    Update index.html, style.css, and app.js with new changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-doocument.getElementById("clickMe").addEventListener("click", function(){
-  alert("Button Clicked!");
-});
+// doocument.getElementById("clickMe").addEventListener("click", function(){
+//   alert("Button Clicked!");
+// });
+
+console.log("Selamat datang di Latihan Git!");
